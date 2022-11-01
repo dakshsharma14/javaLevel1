347dab453866a61d76ca589378612ab11d921a1a
Daksh Sharma <97203868+dakshsharma14@users.noreply.github.com>
2022-11-01 01:46:15 -0400

    Lab 1
    This is lab 1 of computer programming
    Level 1

---
+++ Exercise01.java
@@ -0,0 +1,36 @@
+/*
+ * Student Name: Daksh Sharma
+ * lab Professor: Professor Fedor Ilitchev
+ * Due date: 10th June, 2022
+ * Modified: 9thth June, 2022
+ * Description: Exercise 02
+ */
+
+
+/*Description: This class outputs a tree*/
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		//your code goes here
+		System.out.println("    ~~~~~~~\r\n"    //\r is used as carriage return
+				+ "  ~~~~~~~~~~~\r\n"           //\n is used to move to new line
+				+ "~~~~~~~~~~~~~~~~\r\n"
+				+ "~~~~    ~~~~~\r\n"
+				+ "  ||     ||    ~~~\r\n"
+				+ "   ||   ||    ~~~~~\r\n"
+				+ "     || |     ||\r\n"
+				+ "      | |====||\r\n"
+				+ "      | |\r\n"
+				+ "      |O|\r\n"
+				+ "      | |\r\n"
+				+ "      | |\r\n"
+				+ "     || ||\r\n"
+				+ "****************\r\n"
+				+ "****************\r\n"
+				+ "Program by Daksh Sharma\r\n"
+				+ "");
+		
+
+	}
+
+}
